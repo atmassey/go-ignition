5a2f98c3ad673a6554782bce92a13154da6c9d12
Austin Massey <54543946+atmassey@users.noreply.github.com>
2025-09-05 13:43:32 -0500

    added clock drift events function

--- platform.go
+++ platform.go
@@ -80,7 +80,7 @@ type ClockDrift struct {
 	ClockDrift float32 `json:"clockDrift"`
 }
 
-func (c *Client) ClockDrift() (*ClockDrift, error) {
+func (c *Client) CurrentClockDrift() (*ClockDrift, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("%s/data/api/v1/systemPerformance/driftGauge", c.GetGatewayAddress()), nil)
 	if err != nil {
 		return nil, err
@@ -124,6 +124,36 @@ func (c *Client) ThreadExecutionData() (*ThreadExecutionData, error) {
 		return nil, fmt.Errorf("unexpected status code: %d, status: %v", resp.StatusCode, resp.Status)
 	}
 	var data ThreadExecutionData
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+	return &data, nil
+}
+
+type ClockDriftEvents struct {
+	ClockDriftEvents []struct {
+		Description string `json:"description"`
+		Status      string `json:"status"`
+		Timestamp   int    `json:"timestamp"`
+	} `json:"clockDriftEvents"`
+}
+
+func (c *Client) ClockDriftEvents() (*ClockDriftEvents, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/data/api/v1/systemPerformance/driftEvents", c.GetGatewayAddress()), nil)
+	if err != nil {
+		return nil, err
+	}
+	setHeaders(req, c.Token)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d, status: %v", resp.StatusCode, resp.Status)
+	}
+	var data ClockDriftEvents
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
 		return nil, err
